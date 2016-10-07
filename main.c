15596b9c5494297e982ae21eef7a5d09c31c25ea
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2016-10-07 13:15:19 +0300

    Created command - parameters seperation functionality
    Now the programm can understand which input is the command and which inputs are the parameters

--- main.c
+++ main.c
@@ -11,20 +11,74 @@ void getPrompt(char* buffer){
   getcwd(data, sizeof(data));
   strcat(data, "/$ ");
   strcat(prompt , data);
-
   strcpy(buffer , prompt);
 
 }
 
-int main(){
+void read_command(char* command , char parameters[10][10] , int *number){
+  int i = 0;
+  int j = 0;
+  int first_argument = 0;
+  char *pch;
+  char data[50];
+  fgets(data , sizeof(data) , stdin);
 
+  /* Code for splitting input */
+    pch = strtok (data," \n");
+    while (pch != NULL)
+    {
+      if(first_argument == 0) {
+        //strcat(command , "/bin/");
+        strcpy(command , pch);
+        first_argument = 1;
+      }else{
+      strcpy(parameters[i] , pch);
+      i++;
+      *number = *number + 1;
+    }
+    pch = strtok (NULL, " \n");
+    }
+
+  if(i > 0){
+    do{
+      printf("%s\n" , parameters[j]);
+      j++;
+    }while(j < i);
+  }
+}
+
+int main(){
+/*---------------------------*/
   char prompt[300];
   char data[20];
+  char command[30];
+  char parameters[10][10];
+  int status , i;
+  int numberofparameters = 0;
+/*-----------------------------*/
   getPrompt(prompt);
 
   while(1){
   printf("%s" , prompt);
-  printf("You want to execute %s\n" , data);
+  read_command(command , parameters , &numberofparameters);
+
+  printf("You want to execute %s" , command);
+
+  for(i = 0; i < numberofparameters; i++){
+    printf(" %s " , parameters[i]);
   }
 
+  printf("\n");
+
+  pid_t pid = fork();
+  if(pid == 0){
+    execve(command , parameters , 0);
+    strcpy(command , "");
+    for(i = 0; i < numberofparameters; i++){
+      strcpy(parameters[i] , "");
+    }
+  }else{
+      waitpid(-1, &status, 0);
+    }
+  }
 }
